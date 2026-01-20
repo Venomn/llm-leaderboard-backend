98e240f16799c7fc96ee687cc53240ad8d6c07a0
Venomn <gaobinv@163.com>
2026-01-20 15:19:19 +0800

    change product port to use env

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import modlesRouter from './routes/modles';
 import issuesRouter from './routes/issues';
 // Create Express app
 const app = express();
-const PORT = 3001; // or 3000
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors()); // Enable CORS for all routes
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
